b878177d5381951a190ef7d1c40efedd6d5510a4
Nataliya Gapochka <natasha.gapochka@gmail.com>
2018-04-13 11:19:32 -0700

    config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
 
-// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://cost-curious-api.herokuapp.com';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://cost-curious-api.herokuapp.com';
